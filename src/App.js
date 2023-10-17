4415f328612a6d92583c3a6e84e47883f83811c1
suman <sumobarnwal@gmail.com>
2023-10-17 20:39:25 +0530

    showing all error like no_found,something_wrong,error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, {useState} from 'react';
 import MoviesList from './components/MoviesList';
 import './components/Movie.module.css'
@@ -6,27 +5,19 @@ import './components/Movie.module.css'
 function App() {
         const [movies,setMovies]=useState([]);
         const [isLoading,setIsLoading]=useState(false);
+        const [error,setError]=useState(null);
         
-        const storeinBackend=(e)=>{
-          const moviesId=localStorage.getItem('moviesId')
-          
-          if(moviesId){
-            axios.put('https://crudcrud.com/api/bed7ab0985fc4839a9f99eddf7a40f1f/movie',{
-              current:e
-            })
-             
-          }else{
-          const id=axios.post('https://crudcrud.com/api/bed7ab0985fc4839a9f99eddf7a40f1f/movie',{
-                  current : e
-            })
-            localStorage.setItem('moviesId',id)
-        }
-      }
+         
 
        async function fetchMovieHandler(){
         setIsLoading(true);
-        const response=await fetch('https://swapi.dev/api/films/')
-        const data= await response.json();
+        setError(null);
+        try{
+          const response=await fetch('https://swapi.dev/api/films/')
+          if(!response.ok){
+            throw new Error('Something went wrong')
+          }
+          const data= await response.json();
              
               const transformedMovies=data.results.map((movieData,key)=>{
                  return {
@@ -37,9 +28,12 @@ function App() {
                  }
               })
                setMovies(transformedMovies);
-               storeinBackend(transformedMovies);
-               setIsLoading(false);
+        }catch(error){
+           setError(error.message);
         }
+        setIsLoading(false);
+        }
+         
       
   return (
      <React.Fragment>
@@ -49,6 +43,7 @@ function App() {
       <section>
         {! isLoading && <MoviesList movies={movies}></MoviesList>}
         { isLoading && <img src='https://media.tenor.com/kxZxwvPdm-wAAAAC/loading-loading-screen.gif' width='95%'/>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
      </React.Fragment>
   );
