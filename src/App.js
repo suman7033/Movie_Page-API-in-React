48119b8c14d8794b7fd649b7ff14820a3ea51254
suman <sumobarnwal@gmail.com>
2023-10-17 20:00:20 +0530

    add crudcrud and local store data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, {useState} from 'react';
 import MoviesList from './components/MoviesList';
 import './components/Movie.module.css'
@@ -6,8 +7,23 @@ function App() {
         const [movies,setMovies]=useState([]);
         const [isLoading,setIsLoading]=useState(false);
         
+        const storeinBackend=(e)=>{
+          const moviesId=localStorage.getItem('moviesId')
+          
+          if(moviesId){
+            axios.put('https://crudcrud.com/api/bed7ab0985fc4839a9f99eddf7a40f1f/movie',{
+              current:e
+            })
+             
+          }else{
+          const id=axios.post('https://crudcrud.com/api/bed7ab0985fc4839a9f99eddf7a40f1f/movie',{
+                  current : e
+            })
+            localStorage.setItem('moviesId',id)
+        }
+      }
 
-      async function fetchMovieHandler(){
+       async function fetchMovieHandler(){
         setIsLoading(true);
         const response=await fetch('https://swapi.dev/api/films/')
         const data= await response.json();
@@ -21,6 +37,7 @@ function App() {
                  }
               })
                setMovies(transformedMovies);
+               storeinBackend(transformedMovies);
                setIsLoading(false);
         }
       
@@ -31,7 +48,7 @@ function App() {
       </section>
       <section>
         {! isLoading && <MoviesList movies={movies}></MoviesList>}
-        { isLoading && <img src='https://gifdb.com/images/high/buffering-loading-icon-moving-h9m8b9idk9tpksry.gif'/>}
+        { isLoading && <img src='https://media.tenor.com/kxZxwvPdm-wAAAAC/loading-loading-screen.gif' width='95%'/>}
       </section>
      </React.Fragment>
   );
